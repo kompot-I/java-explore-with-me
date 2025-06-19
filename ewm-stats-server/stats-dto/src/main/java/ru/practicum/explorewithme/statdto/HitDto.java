c7db16f56c61db8a6c778e9008dd075179239afe
Nikita Zubov <n.zubov@lipt-soft.ru>
2025-06-19 18:56:21 +0300

    first-step: fix naming for time stamp.

--- ewm-stats-server/stats-dto/src/main/java/ru/practicum/explorewithme/statdto/HitDto.java
+++ ewm-stats-server/stats-dto/src/main/java/ru/practicum/explorewithme/statdto/HitDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.explorewithme.statdto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,5 +10,6 @@ public class HitDto {
     private String app;
     private String uri;
     private String ip;
-    private String timestamp;
+    @JsonProperty("timestamp")
+    private String creationTime;
 }
